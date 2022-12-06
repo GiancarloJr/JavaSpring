0522941be3bc322f70fc32382f2c369f2ed26267
GiancarloJr <giancarlo456@hotmail.com>
2022-12-06 20:02:24 -0300

    PessoaDTO created

---
+++ backend/src/main/java/ProjectTest/JavaSpring/dto/PessoaDTO.java
@@ -0,0 +1,129 @@
+package ProjectTest.JavaSpring.dto;
+
+import ProjectTest.JavaSpring.entities.Contato;
+import ProjectTest.JavaSpring.entities.Pessoa;
+
+import java.util.List;
+
+public class PessoaDTO {
+
+    private Long id;
+    private String name;
+    private String CPF;
+    private String email;
+    private Long CEP;
+    private String logradouro;
+    private String complemento;
+    private String bairro;
+    private String UF;
+    private List<Contato> contatos;
+
+    public PessoaDTO(){
+
+    }
+
+    public PessoaDTO(Long id, String name, String CPF, String email, Long CEP, String logradouro, String complemento, String bairro, String UF, List<Contato> contatos) {
+        this.id = id;
+        this.name = name;
+        this.CPF = CPF;
+        this.email = email;
+        this.CEP = CEP;
+        this.logradouro = logradouro;
+        this.complemento = complemento;
+        this.bairro = bairro;
+        this.UF = UF;
+        this.contatos = contatos;
+    }
+    public PessoaDTO(Pessoa entity) {
+        this.id = entity.getId();
+        this.name = entity.getName();
+        this.CPF = entity.getCPF();
+        this.email = entity.getEmail();
+        this.CEP = entity.getCEP();
+        this.logradouro = entity.getLogradouro();
+        this.complemento = entity.getComplemento();
+        this.bairro = entity.getBairro();
+        this.UF = entity.getUF();
+        this.contatos = entity.getContatos();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCPF() {
+        return CPF;
+    }
+
+    public void setCPF(String CPF) {
+        this.CPF = CPF;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Long getCEP() {
+        return CEP;
+    }
+
+    public void setCEP(Long CEP) {
+        this.CEP = CEP;
+    }
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getUF() {
+        return UF;
+    }
+
+    public void setUF(String UF) {
+        this.UF = UF;
+    }
+
+    public List<Contato> getContatos() {
+        return contatos;
+    }
+
+    public void setContatos(List<Contato> contatos) {
+        this.contatos = contatos;
+    }
+}
