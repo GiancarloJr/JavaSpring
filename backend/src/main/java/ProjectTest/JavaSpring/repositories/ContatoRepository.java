005c85462a5313e767f3aadf1d9a5e2523f55aee
GiancarloJr <giancarlo456@hotmail.com>
2022-12-06 20:01:02 -0300

    Entities Repository

--- backend/src/main/java/ProjectTest/JavaSpring/repositories/ContatoRepository.java
+++ backend/src/main/java/ProjectTest/JavaSpring/repositories/ContatoRepository.java
@@ -1,4 +1,9 @@
 package ProjectTest.JavaSpring.repositories;
 
-public class ContatoRepository {
+import ProjectTest.JavaSpring.entities.Contato;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ContatoRepository extends JpaRepository<Contato, Long> {
 }
--- backend/src/main/java/ProjectTest/JavaSpring/repositories/PessoaRepository.java
+++ backend/src/main/java/ProjectTest/JavaSpring/repositories/PessoaRepository.java
@@ -1,4 +1,9 @@
 package ProjectTest.JavaSpring.repositories;
 
-public class PessoaRepository {
+import ProjectTest.JavaSpring.entities.Pessoa;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
 }
