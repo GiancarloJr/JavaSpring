b169f2fe45a5ff17fe7708e1e682cb4acd34d260
GiancarloJr <giancarlo456@hotmail.com>
2022-12-06 20:03:29 -0300

    Update and Fix Entity Pessoa

--- backend/src/main/java/ProjectTest/JavaSpring/entities/Contato.java
+++ backend/src/main/java/ProjectTest/JavaSpring/entities/Contato.java
@@ -1,6 +1,9 @@
 package ProjectTest.JavaSpring.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.*;
+import java.util.Objects;
 
 @Entity
 @Table(name = "CONTATOS")
@@ -11,7 +14,7 @@ public class Contato {
     @Column(name = "CON_ID")
     private Long id;
 
-    @Column(name = "CON_NOME")
+    @Column(name = "CON_NAME")
     private String nome;
 
     @Column(name = "CON_TELEFONE")
@@ -20,9 +23,73 @@ public class Contato {
     @Column(name = "CON_CELULAR")
     private Long celular;
 
+    @JsonIgnore
     @ManyToOne
     @JoinColumn(name = "pessoa_id", referencedColumnName = "pes_id")
     private Pessoa pessoa;
 
+    public Contato(){
 
+    }
+
+    public Contato(Long id, String nome, Long telefone, Long celular, Pessoa pessoa) {
+        this.id = id;
+        this.nome = nome;
+        this.telefone = telefone;
+        this.celular = celular;
+        this.pessoa = pessoa;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Long getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(Long telefone) {
+        this.telefone = telefone;
+    }
+
+    public Long getCelular() {
+        return celular;
+    }
+
+    public void setCelular(Long celular) {
+        this.celular = celular;
+    }
+
+    public Pessoa getPessoa() {
+        return pessoa;
+    }
+
+    public void setPessoa(Pessoa pessoa) {
+        this.pessoa = pessoa;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Contato contato = (Contato) o;
+        return id.equals(contato.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
--- backend/src/main/java/ProjectTest/JavaSpring/entities/Pessoa.java
+++ backend/src/main/java/ProjectTest/JavaSpring/entities/Pessoa.java
@@ -1,9 +1,8 @@
 package ProjectTest.JavaSpring.entities;
 
-import org.hibernate.annotations.GeneratorType;
-
 import javax.persistence.*;
 import java.util.List;
+import java.util.Objects;
 
 @Entity
 @Table(name = "PESSOAS")
@@ -17,30 +16,137 @@ public class Pessoa {
     @Column(name = "PES_NAME")
     private String name;
 
-    @Column(name = "PES_CPF")
+    @Column(name = "PES_CPF", unique = true)
     private String CPF;
 
-    @Column(name = "PES_EMAIL")
+    @Column(name = "PES_EMAIL", unique = true)
     private String email;
 
     @Column(name = "PES_CEP")
     private Long CEP;
 
     @Column(name = "PES_LOGRADOURO")
-    private String Logradouro;
+    private String logradouro;
 
     @Column(name = "PES_COMPLEMENTO")
-    private Long Complemento;
+    private String complemento;
 
     @Column(name = "PES_BAIRRO")
-    private String Bairro;
+    private String bairro;
 
     @Column(name = "PES_UF")
     private String UF;
-
-    @Column
+    
     @OneToMany(mappedBy = "pessoa")
     private List<Contato> contatos;
+    
+    public Pessoa(){
+        
+    }
 
+    public Pessoa(Long id, String name, String CPF, String email, Long CEP, String logradouro, String complemento, String bairro, String UF, List<Contato> contatos) {
+        this.id = id;
+        this.name = name;
+        this.CPF = CPF;
+        this.email = email;
+        this.CEP = CEP;
+        this.logradouro = logradouro;
+        this.complemento = complemento;
+        this.bairro = bairro;
+        this.UF = UF;
+        this.contatos = contatos;
+    }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCPF() {
+        return CPF;
+    }
+
+    public void setCPF(String CPF) {
+        this.CPF = CPF;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Long getCEP() {
+        return CEP;
+    }
+
+    public void setCEP(Long CEP) {
+        this.CEP = CEP;
+    }
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getUF() {
+        return UF;
+    }
+
+    public void setUF(String UF) {
+        this.UF = UF;
+    }
+
+    public List<Contato> getContatos() {
+        return contatos;
+    }
+
+    public void setContatos(List<Contato> contatos) {
+        this.contatos = contatos;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Pessoa pessoa = (Pessoa) o;
+        return id.equals(pessoa.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
