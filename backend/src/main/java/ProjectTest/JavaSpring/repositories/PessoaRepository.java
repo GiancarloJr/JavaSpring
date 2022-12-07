a540b9f43b7dae82d8a23c0935238c737135a810
GiancarloJr <giancarlo456@hotmail.com>
2022-12-07 20:38:45 -0300

    PessoaRepository Update

--- backend/src/main/java/ProjectTest/JavaSpring/repositories/PessoaRepository.java
+++ backend/src/main/java/ProjectTest/JavaSpring/repositories/PessoaRepository.java
@@ -4,9 +4,11 @@ import ProjectTest.JavaSpring.entities.Pessoa;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
 
-    Pessoa findByCPF(String cpf);
-    Pessoa findByEmail(String email);
+    Optional<Pessoa> findByCPF(String cpf);
+    Optional<Pessoa> findByEmail(String email);
 }
