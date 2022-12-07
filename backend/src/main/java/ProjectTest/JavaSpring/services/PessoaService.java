8d9e86c3e7416cba7d11729f2c34cf73a7f203a3
GiancarloJr <giancarlo456@hotmail.com>
2022-12-06 23:30:04 -0300

    PessoaServices insert and update

--- backend/src/main/java/ProjectTest/JavaSpring/services/PessoaService.java
+++ backend/src/main/java/ProjectTest/JavaSpring/services/PessoaService.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
@@ -30,5 +31,32 @@ public class PessoaService {
     public List<PessoaDTO> findAll() {
         List<Pessoa> list = pessoaRepository.findAll();
         return list.stream().map(x -> new PessoaDTO(x)).collect(Collectors.toList());
+    }
+
+    public PessoaDTO insert(PessoaDTO pessoaDTO) {
+            Pessoa entity = new Pessoa();
+            convertDTOtoEntity(entity,pessoaDTO);
+            return new PessoaDTO(pessoaRepository.save(entity));
+    }
+
+    public PessoaDTO update(Long id,PessoaDTO pessoaDTO) {
+        try {
+            Optional<Pessoa> entity = pessoaRepository.findById(id);
+            convertDTOtoEntity(entity.get(), pessoaDTO);
+            return new PessoaDTO(pessoaRepository.save(entity.get()));
+        } catch (NoSuchElementException e){
+            throw new NotFoundException("Pessoa não encontrada");
+        }
+    }
+
+    public void convertDTOtoEntity(Pessoa entity,PessoaDTO pessoaDTO){
+        entity.setName(pessoaDTO.getName());
+        entity.setBairro(pessoaDTO.getBairro());
+        entity.setCEP(pessoaDTO.getCEP());
+        entity.setComplemento(pessoaDTO.getComplemento());
+        entity.setCPF(pessoaDTO.getCPF());
+        entity.setLogradouro(pessoaDTO.getLogradouro());
+        entity.setEmail(pessoaDTO.getEmail());
+        entity.setUF(pessoaDTO.getUF());
     }
 }
