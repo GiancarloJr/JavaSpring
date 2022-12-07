c756c5fb379495096a86bbdd11274ec0609926c2
GiancarloJr <giancarlo456@hotmail.com>
2022-12-07 11:21:25 -0300

    Custom Valiation's Exceptions

---
+++ backend/src/main/java/ProjectTest/JavaSpring/services/exceptions/DataBaseException.java
@@ -0,0 +1,8 @@
+package ProjectTest.JavaSpring.services.exceptions;
+
+public class DataBaseException extends RuntimeException{
+
+    public DataBaseException(String message) {
+        super(message);
+    }
+}
--- backend/src/main/java/ProjectTest/JavaSpring/services/exceptions/ResourceExceptionHandler.java
+++ backend/src/main/java/ProjectTest/JavaSpring/services/exceptions/ResourceExceptionHandler.java
@@ -2,10 +2,13 @@ package ProjectTest.JavaSpring.services.exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.validation.ConstraintViolationException;
 import java.time.Instant;
 
 @ControllerAdvice
@@ -20,6 +23,20 @@ public class ResourceExceptionHandler {
             error.setPath(request.getRequestURI());
 
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+
+    @ExceptionHandler({MethodArgumentNotValidException.class})
+    public ResponseEntity<ValidationError> Validation(MethodArgumentNotValidException e, HttpServletRequest request){
+        ValidationError error = new ValidationError();
+        error.setTimestamp(Instant.now());
+        error.setStatus(HttpStatus.UNPROCESSABLE_ENTITY.value());
+        error.setMessage(e.getMessage());
+        error.setPath(request.getRequestURI());
+        for(FieldError f: e.getBindingResult().getFieldErrors()){
+            error.addError(f.getField(), f.getDefaultMessage());
+        }
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
     }
 
 }
